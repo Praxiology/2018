36caac7568969bc0f451f2c92f3b8fbbf1f63a6f
jsonkv <860492946@qq.com>
2018-03-04 12:05:19 +0800

    算法和数据结构汇总学习记录

---
+++ javabase/javabase/src/javabase/step/alg/MaoPaoDXSort.java
@@ -0,0 +1,51 @@
+package javabase.step.alg;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+//定向冒泡排序(双向排序)
+public class MaoPaoDXSort {
+
+	//
+	public static int[] sortToBg(int[] or) {
+		int ol = or.length;
+		for (int o = 0; o < ol; o++) {
+			int temp = 0;
+			// 冒泡从左到右
+			int irs = o + 1;// 从左到右比较的开始位置
+			int ire = ol - o;// 从左到右比较的结束位置
+			if (irs >= ire) {
+				System.out.println("1");
+				break;
+			}
+			for (int ir = irs; ir < ire; ir++) {
+				if (or[ir - 1] > or[ir]) {
+					temp = or[ir];
+					or[ir] = or[ir - 1];
+					or[ir - 1] = temp;
+				}
+			}
+			// 冒泡从到右到左
+			int ils = ol - o - 2;// 从右到左比较的开始位置
+			int ile = o;// 从右到左比较的结束位置
+			if (ils <= ile) {
+				System.out.println("2");
+				break;
+			}
+			for (int il = ils; il > ile; il--) {
+				if (or[il - 1] > or[il]) {
+					temp = or[il];
+					or[il] = or[il - 1];
+					or[il - 1] = temp;
+				}
+			}
+		}
+		return or;
+	}
+	@Test
+	public void testMP() {
+		int [] rel = MaoPaoDXSort.sortToBg(new int[] {9,5,10,7,3,4,11,2,1,0,8});
+		System.out.println(Arrays.toString(rel));
+	}
+}
